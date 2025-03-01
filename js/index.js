c4b308c7cb9bef6a1d01a2b652ada97d88e163db
muntasirtanjil <muntasirtanjil9@gmail.com>
2025-03-01 23:38:09 +0600

    layout section complete

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.getElementById('back-btn').addEventListener('click',function(event){
+
+    window.location.href ="index.html"
+})
+
+document.getElementById('discover').addEventListener('click',function(event){
+
+    window.location.href ="next.html"
+})
